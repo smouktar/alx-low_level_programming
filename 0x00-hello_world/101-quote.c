09e6e2cbf6e132a2dbecb1aee42f73b712f13fd8
smouktar <sulymanmouktar@yahoo.com>
2023-09-07 14:07:43 -0700

    added tedious putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putc('a',stderr);
+	putc('n',stderr);
+	putc('d',stderr);
+	putc(' ',stderr);
+	putc('t',stderr);
+	putc('h',stderr);
+	putc('a',stderr);
+	putc('t',stderr);
+	putc(' ',stderr);
+	putc('p',stderr);
+	putc('i',stderr);
+	putc('e',stderr);
+	putc('c',stderr);
+	putc('e',stderr);
+	putc(' ',stderr);
+	putc('o',stderr);
+	putc('f',stderr);
+	putc(' ',stderr);
+	putc('a',stderr);
+	putc('r',stderr);
+	putc('t',stderr);
+	putc(' ',stderr);
+	putc('i',stderr);
+	putc('s',stderr);
+	putc(' ',stderr);
+	putc('u',stderr);
+	putc('s',stderr);
+	putc('e',stderr);
+	putc('f',stderr);
+	putc('u',stderr);
+	putc('l',stderr);
+	putc('"',stderr);
+	putc(' ',stderr);
+	putc('-',stderr);
+	putc(' ',stderr);
+	putc('D',stderr);
+	putc('o',stderr);
+	putc('r',stderr);
+	putc('a',stderr);
+	putc(' ',stderr);
+	putc('K',stderr);
+	putc('o',stderr);
+	putc('r',stderr);
+	putc('p',stderr);
+	putc('a',stderr);
+	putc('r',stderr);
+	putc(',',stderr);
+	putc(' ',stderr);
+	putc('2',stderr);
+	putc('0',stderr);
+	putc('1',stderr);
+	putc('5',stderr);
+	putc('-',stderr);
+	putc('1',stderr);
+	putc('0',stderr);
+	putc('-',stderr);
+	putc('1',stderr);
+	putc('9',stderr);
+	putc('\n',stderr);
+
+    return (0);
+}
