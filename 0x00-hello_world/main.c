46a8e3cc6a119b5bda54bf85dc48f2261a29eeee
smouktar <sulymanmouktar@yahoo.com>
2023-09-07 10:18:35 -0700

    edited file 100

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,6 +5,5 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    return (0);
 }
