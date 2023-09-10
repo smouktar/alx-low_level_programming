a5ddbe53f51ec6d979ed3d1b79455b1980445b4e
smouktar <sulymanmouktar@yahoo.com>
2023-09-10 08:08:52 -0700

    added 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first = 'a';
+
+	while (first < 'z')
+	{
+		putchar(first);
+		first++;
+	}
+	putchar('\n');
+	return (0);
+}
