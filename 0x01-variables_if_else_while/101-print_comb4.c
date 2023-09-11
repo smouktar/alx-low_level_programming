2ba312d28d62335d09eb112f234abfe827d73910
smouktar <sulymanmouktar@yahoo.com>
2023-09-11 07:36:08 -0700

    Edited 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,21 +16,20 @@ int main(void)
 	{
 		while (second <= '9')
 		{
-			while(third <= '9')
+			while (third <= '9')
 			{
-			
 				if (first < second && second < third)
 				{
 					putchar(first);
 					putchar(second);
 					putchar(third);
 
-					if (first < '7' || second < '8'|| third != '9')
-					{		
+					if (first < '7' || second < '8' || third != '9')
+					{
 						putchar(comma);
 						putchar(space);
 					}
-				}	
+				}
 				third++;
 			}
 			second++;
