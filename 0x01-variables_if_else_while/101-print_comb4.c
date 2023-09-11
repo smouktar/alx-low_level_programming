2d7912cfbb15e685a8d010bffa3505c5eea88748
smouktar <sulymanmouktar@yahoo.com>
2023-09-11 06:23:50 -0700

    added 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first = '0';
+	int comma = ',';
+	int space = ' ';
+	int second = '0';
+	int third = '0';
+
+	while (first <= '9')
+	{
+		while (second <= '9')
+		{
+			while(third <= '9')
+			{
+			
+				if (first < second && second < third)
+				{
+					putchar(first);
+					putchar(second);
+					putchar(third);
+
+					if (first < '7' || second < '8'|| third != '9')
+					{		
+						putchar(comma);
+						putchar(space);
+					}
+				}	
+				third++;
+			}
+			second++;
+			third = '0';
+		}
+		first++;
+		second = '0';
+	}
+	putchar('\n');
+	return (0);
+}
