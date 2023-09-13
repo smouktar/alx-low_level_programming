58a7347386a4475a27df5871ab959dc254ed0e1c
smouktar <sulymanmouktar@yahoo.com>
2023-09-13 03:55:42 -0700

    add nested if and loo

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * print_to_98  - Print all natural numbers and end with Ninety-eight
+ * @n: The number to be checked
+ *
+ * Description: print series of numbers that end  with Ninety_eight
+ * Numbers must be separated by a comma, followed by a space
+ * Numbers should be printed in order
+ * The first printed number should be the number passed to your function
+ * The last printed number should be 98
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+}
+
