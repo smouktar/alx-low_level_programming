2ebc68f40cd4d68cb94eb23ed949174444d1668d
smouktar <sulymanmouktar@yahoo.com>
2023-09-14 06:25:27 -0700

    corrected files

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,5 @@
 int _putchar(char c);
+void positive_or_negative(int i);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * positive_or_negative - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative (int i)
+{
+	/* your code goes there */
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+
+}
