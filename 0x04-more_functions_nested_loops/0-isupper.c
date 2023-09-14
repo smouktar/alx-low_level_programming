f38bc443479b9bc825218ac4a3ff63901afe6e93
smouktar <sulymanmouktar@yahoo.com>
2023-09-14 10:43:25 -0700

    edited 0-

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * int _isupper - Short description, single line
+ * _isupper - Short description, single line
  * @c: only parameter
  *
- * Description: Checks if argument is upper
- * section header: Section description)*
+ * Description: Checks if argument is upper case
+ *
  * Return: (1) or (0)
  */
 int _isupper(int c)
