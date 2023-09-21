d374850843ebef5ab370cf8dfcb71b3639fc6478
smouktar <sulymanmouktar@yahoo.com>
2023-09-21 00:58:07 -0700

    added all 0x05

--- 0x05-pointers_arrays_strings/6-main.c
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * main - check the code
  *
@@ -8,10 +8,8 @@
 int main(void)
 {
     char *str;
-    int len;
 
-    str = "My first strlen!";
-    len = len(str);
-    printf("%d\n", len);
+    str = "0123456789";
+    puts2(str);
     return (0);
 }
