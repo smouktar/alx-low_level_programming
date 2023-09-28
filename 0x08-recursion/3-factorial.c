d8da82eb15b7aa3cc61de7f75c8b52f46af0c07c
smouktar <sulymanmouktar@yahoo.com>
2023-09-28 09:11:07 -0700

    modified recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * factorial - Calculate the factorial of a number
--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 int check_palindrome(char *s);
 
