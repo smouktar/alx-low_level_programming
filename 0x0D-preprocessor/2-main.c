bd9dbf557ceef830c81c15667c2ecc39b5613ff3
smouktar <sulymanmouktar@yahoo.com>
2023-10-11 06:09:50 -0700

    modified 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**
- * main - main program
- */
 
-void main(void)
+/**
+  * main - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+int main(void)
 {
 	printf("%s\n", __FILE__);
-
+	return (0);
 }
